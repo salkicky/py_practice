f3958db99290838ffa587a9a6d2edbfb6bfac7f7
salkicky <salkicky@gmail.com>
2012-06-12 16:28:43 +0900

    add ConfigParser sample

---
+++ configure/config_sample.py
@@ -0,0 +1,12 @@
+#encoding:shift-jis
+
+import ConfigParser
+
+if __name__ == "__main__":
+    inifile = ConfigParser.SafeConfigParser()
+    inifile.read(u'sample.ini')
+
+    print inifile.get("index1","param2")
+    print inifile.get("index1","param1")
+    print inifile.get("index2","param3")
+    print inifile.get("index2","ƒpƒ‰ƒ[ƒ^‚S")
